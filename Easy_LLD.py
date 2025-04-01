59a18afba56203046606114d25391dfcabe800af
Giang Nguyen <giangnguyen321@gmail.com>
2025-04-01 15:46:48 -0400

    creating the job_materials repo

---
+++ Easy_LLD.py
@@ -0,0 +1,3 @@
+# source: https://www.finalroundai.com/blog/low-level-design-interview-questions
+# https://www.geeksforgeeks.org/top-10-system-design-interview-questions-and-answers/
+# https://github.com/ashishps1/awesome-low-level-design
---
+++ web_crawler.py
@@ -0,0 +1,12 @@
+'''
+webcrawler: (also known as a spider) is a system for downloading, storing, and analyzing web pages. 
+It performs the task of organizing web pages so that users can easily find the information.
+
+source: https://www.enjoyalgorithms.com/blog/web-crawler
+
+REQUIREMENTS FOR THE WEB CRAWLER
+1. Right Cadence
+2. Duplicate Web Pages
+3. Crawling Permission 
+
+'''
